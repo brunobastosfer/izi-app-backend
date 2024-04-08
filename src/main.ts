957d10acc694b56fe80f528bb78318a1525ebdf7
Bruno Bastos <brunobastos@edu.unifor.br>
2024-04-08 18:52:36 -0300

    feat: adicionando o prisma, swagger e dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('API IZI APP')
+    .setDescription('Documentação da API')
+    .setVersion('1.0')
+    .addTag('nest')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
