c644085cb08a941c6037958253cc8513450f017d
Bruno Bastos <brunobastos@edu.unifor.br>
2024-04-08 18:53:20 -0300

    feat: alterando a mensagem no service app

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'IZI APP API Is RUNNING!';
   }
 }
